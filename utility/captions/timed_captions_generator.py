dc27d9ce1fac5daf53af0db287c02ba1dfc2c2b8
joerigh <joerigh@yahoo.com>
2025-09-27 20:25:40 +0700

    Update timed_captions_generator.py

--- utility/captions/timed_captions_generator.py
+++ utility/captions/timed_captions_generator.py
@@ -1,79 +1,74 @@
-import re
+# utility/captions/timed_caption_generator.py
+
+import whisper_timestamped as whisper
 from whisper_timestamped import load_model, transcribe_timestamped
+import re
 
 def generate_timed_captions(audio_filename, model_size="base"):
-    # Load model Whisper
+    """
+    Generate timed captions from audio file using whisper-timestamped.
+    Returns: list of tuples [((start_time, end_time), text), ...]
+    """
     WHISPER_MODEL = load_model(model_size)
+    result = transcribe_timestamped(WHISPER_MODEL, audio_filename, verbose=False, fp16=False)
+    return get_captions_with_time(result)
 
-    # Transcribe audio dengan timestamp
-    gen = transcribe_timestamped(WHISPER_MODEL, audio_filename, verbose=False, fp16=False)
+# -------------------------
+# Helper functions
+# -------------------------
 
-    # Ambil captions
-    captions_raw = getCaptionsWithTime(gen)
-
-    # Convert ke dict biar kompatibel dengan pipeline
-    captions = []
-    for (start, end), text in captions_raw:
-        captions.append({
-            "start": float(start),
-            "end": float(end),
-            "text": text
-        })
-
-    return captions
-
-def splitWordsBySize(words, maxCaptionSize):
-    halfCaptionSize = maxCaptionSize / 2
+def split_words_by_size(words, max_caption_size):
+    half_caption_size = max_caption_size / 2
     captions = []
     while words:
         caption = words[0]
         words = words[1:]
-        while words and len(caption + ' ' + words[0]) <= maxCaptionSize:
+        while words and len(caption + ' ' + words[0]) <= max_caption_size:
             caption += ' ' + words[0]
             words = words[1:]
-            if len(caption) >= halfCaptionSize and words:
+            if len(caption) >= half_caption_size and words:
                 break
         captions.append(caption)
     return captions
 
-def getTimestampMapping(whisper_analysis):
+def get_timestamp_mapping(whisper_analysis):
     index = 0
-    locationToTimestamp = {}
+    location_to_timestamp = {}
     for segment in whisper_analysis['segments']:
         for word in segment['words']:
-            newIndex = index + len(word['text'])+1
-            locationToTimestamp[(index, newIndex)] = word['end']
-            index = newIndex
-    return locationToTimestamp
+            new_index = index + len(word['text']) + 1
+            location_to_timestamp[(index, new_index)] = word['end']
+            index = new_index
+    return location_to_timestamp
 
-def cleanWord(word):
+def clean_word(word):
     return re.sub(r'[^\w\s\-_"\'\']', '', word)
 
-def interpolateTimeFromDict(word_position, d):
+def interpolate_time_from_dict(word_position, d):
     for key, value in d.items():
         if key[0] <= word_position <= key[1]:
             return value
     return None
 
-def getCaptionsWithTime(whisper_analysis, maxCaptionSize=15, considerPunctuation=False):
-    wordLocationToTime = getTimestampMapping(whisper_analysis)
+def get_captions_with_time(whisper_analysis, max_caption_size=15, consider_punctuation=False):
+    word_loc_to_time = get_timestamp_mapping(whisper_analysis)
     position = 0
     start_time = 0
-    CaptionsPairs = []
+    captions_pairs = []
     text = whisper_analysis['text']
-    
-    if considerPunctuation:
+
+    if consider_punctuation:
         sentences = re.split(r'(?<=[.!?]) +', text)
-        words = [word for sentence in sentences for word in splitWordsBySize(sentence.split(), maxCaptionSize)]
+        words = [word for sentence in sentences for word in split_words_by_size(sentence.split(), max_caption_size)]
     else:
         words = text.split()
-        words = [cleanWord(word) for word in splitWordsBySize(words, maxCaptionSize)]
-    
+        words = [clean_word(word) for word in split_words_by_size(words, max_caption_size)]
+
     for word in words:
         position += len(word) + 1
-        end_time = interpolateTimeFromDict(position, wordLocationToTime)
+        end_time = interpolate_time_from_dict(position, word_loc_to_time)
         if end_time and word:
-            CaptionsPairs.append(((start_time, end_time), word))
+            captions_pairs.append(((start_time, end_time), word))
             start_time = end_time
 
-    return CaptionsPairs
+    return captions_pairs
