b7dc1223b8139db57b392885d5847e9a8e95a222
joerigh <joerigh@yahoo.com>
2025-09-27 10:51:11 +0700

    Update install_moviepy.py

--- install_moviepy.py
+++ install_moviepy.py
@@ -1,6 +1,1 @@
-ModuleNotFoundError: No module named 'moviepy.editor'
-Traceback:
-File "E:\Easy-Text-To-Video-AI-main\Easy-Text-To-Video-AI-main\streamlit_main.py", line 7, in <module>
-    from utility.render.render_engine import get_output_media
-File "E:\Easy-Text-To-Video-AI-main\Easy-Text-To-Video-AI-main\utility\render\render_engine.py", line 4, in <module>
-    from moviepy.editor import AudioFileClip, CompositeVideoClip, CompositeAudioClip, VideoFileClip, TextClip
+DLzD281196wa1jxs3Fq2yVITUnt9NPTV3FUcpykXqBv1p9tzOLRMnIqH
