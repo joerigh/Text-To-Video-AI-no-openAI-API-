ea1585af2eb43f959e533291b4d55117b9d57017
joerigh <joerigh@yahoo.com>
2025-09-27 20:18:58 +0700

    Delete utility/audio directory

--- utility/audio/audio_generator.py
+++
@@ -1,7 +0,0 @@
-import edge_tts
-import asyncio
-
-async def generate_audio(text, output_file="audio_tts.wav", voice="id-ID-ArdiNeural"):
-    communicate = edge_tts.Communicate(text, voice)
-    await communicate.save(output_file)
-    print(f"Audio berhasil dibuat: {output_file}")
--- utility/audio/tts.py
+++
@@ -1,9 +0,0 @@
-import edge_tts
-import asyncio
-
-async def _generate_tts_async(text, output_file):
-    communicate = edge_tts.Communicate(text, voice="id-ID-ArdiNeural")
-    await communicate.save(output_file)
-
-def generate_audio(text, output_file="audio_tts.wav"):
-    asyncio.run(_generate_tts_async(text, output_file))
