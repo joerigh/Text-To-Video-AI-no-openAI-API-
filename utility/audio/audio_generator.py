abb541be2071977f53ebf39c0f8d36e132ea253a
joerigh <joerigh@yahoo.com>
2025-09-27 22:26:13 +0700

    Update audio_generator.py

--- utility/audio/audio_generator.py
+++ utility/audio/audio_generator.py
@@ -1,10 +1,23 @@
 import edge_tts
 import asyncio
+from langdetect import detect
 
-async def generate_audio(text, outputFilename, voice="en-AU-WilliamNeural"):
+async def generate_audio(text, outputFilename, auto_detect=True):
     """
     Generate TTS audio using edge-tts.
-    voice: pilih voice sesuai bahasa ('en-AU-WilliamNeural' atau 'id-ID-GadisNeural')
+
+    Parameters:
+    - text: script teks
+    - outputFilename: file output mp3
+    - auto_detect: jika True, voice akan dipilih sesuai bahasa (ID/EN)
     """
+
+    # Pilih voice
+    if auto_detect:
+        lang_code = detect(text)  # 'id' = Indonesian, 'en' = English
+        voice = "id-ID-GadisNeural" if lang_code == "id" else "en-AU-WilliamNeural"
+    else:
+        voice = "en-AU-WilliamNeural"
+
     communicate = edge_tts.Communicate(text, voice)
     await communicate.save(outputFilename)
