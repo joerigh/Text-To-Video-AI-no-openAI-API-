6b996e31ef1ba8239b52658af1e1b37cfc744629
joerigh <joerigh@yahoo.com>
2025-09-27 20:17:00 +0700

    Update utils.py

--- utility/utils.py
+++ utility/utils.py
@@ -3,32 +3,32 @@ from datetime import datetime
 import json
 
 # Log types
-LOG_TYPE_GPT = "GPT"
+LOG_TYPE_SCRIPT = "SCRIPT"
 LOG_TYPE_PEXEL = "PEXEL"
 
-# log directory paths
-DIRECTORY_LOG_GPT = ".logs/gpt_logs"
+# Log directory paths
+DIRECTORY_LOG_SCRIPT = ".logs/script_logs"
 DIRECTORY_LOG_PEXEL = ".logs/pexel_logs"
 
-# method to log response from pexel and openai
-def log_response(log_type, query,response):
+def log_response(log_type, query, response):
     log_entry = {
         "query": query,
         "response": response,
         "timestamp": datetime.now().isoformat()
     }
-    if log_type == LOG_TYPE_GPT:
-        if not os.path.exists(DIRECTORY_LOG_GPT):
-            os.makedirs(DIRECTORY_LOG_GPT)
-        filename = '{}_gpt3.txt'.format(datetime.now().strftime("%Y%m%d_%H%M%S"))
-        filepath = os.path.join(DIRECTORY_LOG_GPT, filename)
-        with open(filepath, "w") as outfile:
-            outfile.write(json.dumps(log_entry) + '\n')
+
+    if log_type == LOG_TYPE_SCRIPT:
+        if not os.path.exists(DIRECTORY_LOG_SCRIPT):
+            os.makedirs(DIRECTORY_LOG_SCRIPT)
+        filename = '{}_script.txt'.format(datetime.now().strftime("%Y%m%d_%H%M%S"))
+        filepath = os.path.join(DIRECTORY_LOG_SCRIPT, filename)
+        with open(filepath, "w") as f:
+            f.write(json.dumps(log_entry) + '\n')
 
     if log_type == LOG_TYPE_PEXEL:
         if not os.path.exists(DIRECTORY_LOG_PEXEL):
             os.makedirs(DIRECTORY_LOG_PEXEL)
         filename = '{}_pexel.txt'.format(datetime.now().strftime("%Y%m%d_%H%M%S"))
         filepath = os.path.join(DIRECTORY_LOG_PEXEL, filename)
-        with open(filepath, "w") as outfile:
-            outfile.write(json.dumps(log_entry) + '\n')
+        with open(filepath, "w") as f:
+            f.write(json.dumps(log_entry) + '\n')
